5902fc0ca08af20f014384b2282f173df98206f3
Seth T. <crimsonknightstudios@gmail.com>
2025-07-22 13:25:43 -0400

    Removes pain points of a monkey patch that allows HTTP Method Tunneling, but causes other issues with HTTP requests when using external APIs (#1370)

--- src/amber/router/request.cr
+++ src/amber/router/request.cr
@@ -6,49 +6,56 @@ class HTTP::Request
   METHOD          = "_method"
   OVERRIDE_HEADER = "X-HTTP-Method-Override"
 
+  # Required for the `matched_route` method
   @matched_route : Amber::Router::RoutedResult(Amber::Route)?
-  @requested_method : String?
+  
+  # Required for the `params` method
   @params : Amber::Router::Params?
 
-  def method
-    case @method
-    when "POST" then requested_method.to_s.upcase
-    else             @method
-    end
-  end
-
-  def requested_method
-    @requested_method ||= params.override_method?(METHOD) || headers[OVERRIDE_HEADER]? || @method
-  end
-
+  # A necessary method that the rest of the Amber server requires to be present
+  # TODO: Refactor this into a different approach that doesn't require monkey patching the std lib
   def params
     @params ||= Amber::Router::Params.new(self)
   end
 
+  # This is a necessary method that the rest of the Amber server requires to be present
+  # TODO: Refactor this into a different approach that doesn't require monkey patching the std lib
   def port
     uri.port
   end
 
+  # This is a necessary method that the rest of the Amber server requires to be present
+  # TODO: Refactor this into a different approach that doesn't require monkey patching the std lib
   def url
     uri.to_s
   end
 
+  # This is a necessary method that the rest of the Amber server requires to be present
+  # TODO: Refactor this into a different approach that doesn't require monkey patching the std lib
   def route
     matched_route.payload.not_nil!
   end
 
+  # This is a necessary method that the rest of the Amber server requires to be present
+  # TODO: Refactor this into a different approach that doesn't require monkey patching the std lib
   def valid_route?
     matched_route.payload? || router.socket_route_defined?(self)
   end
 
+  # This is a necessary method that the rest of the Amber server requires to be present
+  # TODO: Refactor this into a different approach that doesn't require monkey patching the std lib
   def process_websocket
     router.get_socket_handler(self)
   end
 
+  # This is a necessary method that the rest of the Amber server requires to be present
+  # TODO: Refactor this into a different approach that doesn't require monkey patching the std lib
   def matched_route
     @matched_route ||= router.match_by_request(self)
   end
 
+  # This is a necessary method that the rest of the Amber server requires to be present
+  # TODO: Refactor this into a different approach that doesn't require monkey patching the std lib
   private def router
     Amber::Server.router
   end
